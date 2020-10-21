36ccd64e861cff7e8a7eeb189453ca06473af3a5
Dr_wolf <drwolf612@gmail.com>
2020-10-21 15:32:37 +0100

    added finally

---
+++ main.js
@@ -0,0 +1,71 @@
+$(".lin").hide();
+
+//$(".ep0").hide();
+
+/*$(document).ready(function () {
+  $(".myButton").click(function () {
+    $(".ep0").toggle();
+  });
+});*/
+
+
+$(document).ready(function () {
+  $(".btnall").click(function () {
+    $(".linn").toggle();
+  });
+});
+
+$(document).ready(function () {
+  $(".btn1").click(function () {
+    $(".linn1").toggle(1000);
+  });
+});
+$(document).ready(function () {
+  $(".btn2").click(function () {
+    $(".linn2").toggle(1000);
+  });
+});
+$(document).ready(function () {
+  $(".btn3").click(function () {
+    $(".linn3").toggle(1000);
+  });
+});
+$(document).ready(function () {
+  $(".btn4").click(function () {
+    $(".linn4").toggle(1000);
+  });
+});
+$(document).ready(function () {
+  $(".btn5").click(function () {
+    $(".linn5").toggle(1000);
+  });
+});
+$(document).ready(function () {
+  $(".btn6").click(function () {
+    $(".linn6").toggle(1000);
+  });
+});
+$(document).ready(function () {
+  $(".btn7").click(function () {
+    $(".linn7").toggle(1000);
+  });
+});
+$(document).ready(function () {
+  $(".btn8").click(function () {
+    $(".linn8").toggle(1000);
+  });
+});
+$(document).ready(function () {
+  $(".btn9").click(function () {
+    $(".linn9").toggle(1000);
+  });
+});
+$(document).ready(function () {
+  $(".btn10").click(function () {
+    $(".linn10").toggle(1000);
+  });
+});
+
+
+
+
