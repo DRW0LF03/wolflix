f1dcae251beed767debba835362cb7002a701956
Dr_wolf <drwolf612@gmail.com>
2020-12-05 16:28:53 +0100

    add new serie

--- main.js
+++ main.js
@@ -5,82 +5,134 @@ $(".frm").hide();
 $(".hidmov").hide();
 
 
-$(document).ready(function () {
-  $(".curr").click(function () {
-    $(".hidmov").show();
-  });
+
+//$(document).ready(function() {
+//   $(".showwbutton").click(function() {
+//      $(".btn").show();
+//   });
+//});
+
+$(document).ready(function() {
+    $(".").click(function() {
+        $(".").toggle();
+    });
+});
+
+
+
+$(document).ready(function() {
+    $(".mButton").click(function() {
+        $(".frm").show();
+    });
+});
+
+
+$(document).ready(function() {
+    $(".myButtonn1").click(function() {
+        $(".lin1").toggle(1000);
+    });
+});
+$(document).ready(function() {
+    $(".myButtonn1").click(function() {
+        $(".lin2").toggle(1000);
+    });
+});
+$(document).ready(function() {
+    $(".myButtonn1").click(function() {
+        $(".lin3").toggle(1000);
+    });
+});
+$(document).ready(function() {
+    $(".myButtonn1").click(function() {
+        $(".lin4").toggle(1000);
+    });
+});
+$(document).ready(function() {
+    $(".myButtonn1").click(function() {
+        $(".lin5").toggle(1000);
+    });
+});
+$(document).ready(function() {
+    $(".myButtonn1").click(function() {
+        $(".lin6").toggle(1000);
+    });
+});
+$(document).ready(function() {
+    $(".myButtonn1").click(function() {
+        $(".lin7").toggle(1000);
+    });
+});
+$(document).ready(function() {
+    $(".myButtonn1").click(function() {
+        $(".lin8").toggle(1000);
+    });
+});
+$(document).ready(function() {
+    $(".myButtonn1").click(function() {
+        $(".lin9").toggle(1000);
+    });
+});
+$(document).ready(function() {
+    $(".myButtonn1").click(function() {
+        $(".lin10").toggle(1000);
+    });
 });
 
 
 
 
 
-
-$(document).ready(function () {
-  $(".myButton").click(function () {
-    $(".frm").show();
-  });
+/*season 02 */
+$(document).ready(function() {
+    $(".myButtonn2").click(function() {
+        $(".linn1").toggle(1000);
+    });
 });
-
-
-$(document).ready(function () {
-  $(".btnall").click(function () {
-    $(".linn").toggle();
-  });
+$(document).ready(function() {
+    $(".myButtonn2").click(function() {
+        $(".linn2").toggle(1000);
+    });
 });
-
-$(document).ready(function () {
-  $(".btn1").click(function () {
-    $(".linn1").toggle(1000);
-  });
+$(document).ready(function() {
+    $(".myButtonn2").click(function() {
+        $(".linn3").toggle(1000);
+    });
 });
-$(document).ready(function () {
-  $(".btn2").click(function () {
-    $(".linn2").toggle(1000);
-  });
+$(document).ready(function() {
+    $(".myButtonn2").click(function() {
+        $(".linn4").toggle(1000);
+    });
 });
-$(document).ready(function () {
-  $(".btn3").click(function () {
-    $(".linn3").toggle(1000);
-  });
+$(document).ready(function() {
+    $(".myButtonn2").click(function() {
+        $(".linn5").toggle(1000);
+    });
 });
-$(document).ready(function () {
-  $(".btn4").click(function () {
-    $(".linn4").toggle(1000);
-  });
+$(document).ready(function() {
+    $(".myButtonn2").click(function() {
+        $(".linn6").toggle(1000);
+    });
 });
-$(document).ready(function () {
-  $(".btn5").click(function () {
-    $(".linn5").toggle(1000);
-  });
+$(document).ready(function() {
+    $(".myButtonn2").click(function() {
+        $(".linn7").toggle(1000);
+    });
 });
-$(document).ready(function () {
-  $(".btn6").click(function () {
-    $(".linn6").toggle(1000);
-  });
+$(document).ready(function() {
+    $(".myButtonn2").click(function() {
+        $(".linn8").toggle(1000);
+    });
 });
-$(document).ready(function () {
-  $(".btn7").click(function () {
-    $(".linn7").toggle(1000);
-  });
+$(document).ready(function() {
+    $(".myButtonn2").click(function() {
+        $(".linn9").toggle(1000);
+    });
 });
-$(document).ready(function () {
-  $(".btn8").click(function () {
-    $(".linn8").toggle(1000);
-  });
+$(document).ready(function() {
+    $(".myButtonn2").click(function() {
+        $(".linn10").toggle(1000);
+    });
 });
-$(document).ready(function () {
-  $(".btn9").click(function () {
-    $(".linn9").toggle(1000);
-  });
-});
-$(document).ready(function () {
-  $(".btn10").click(function () {
-    $(".linn10").toggle(1000);
-  });
-});
-
-
 
 
 /*$(document).ready(function () {
